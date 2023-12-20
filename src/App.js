206702de4755b2e9241aba72f035b92f7703a161
classwwork <vikasroy1307@gmail.com>
2023-12-20 17:15:28 +0530

    vikas roy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './Component/Card';
+import cardContent from './Util/data.json'
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      
+    {
+    cardContent.map((cardContent)=>{
+     return <Card title={cardContent.title} subTitle={cardContent.subtitle}/>
+})
+
+    
+}
     </div>
   );
 }
